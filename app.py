91cf34985e8041f145550be72f60cf0d7cb390d7
Maneechi <51394021+Maneechi@users.noreply.github.com>
2022-05-01 11:41:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,10 +31,7 @@ def main():
     
         # Render the form again, but add in the prediction and remind user
         # of the values they input before
-        return flask.render_template('main.html',
-                                     original_input={'string':string},
-                                     result=prediction,
-                                    )
+        return flask.render_template('main.html', original_input={'string':string}, result=prediction)
 
 if __name__ == '__main__':
     app.run()
